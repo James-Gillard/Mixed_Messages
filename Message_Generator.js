d7dfd526ffb4b251901bd53b3404db2dcf18895d
Jame Gillard <jame-gillard@hotmail.com>
2022-12-13 14:28:47 +0000

    first commit take 2

--- Message_Generator.js
+++ Message_Generator.js
@@ -1,1 +1,21 @@
-console.log("Hello")
+//You are the doing a great job.
+//No matter what adversities come your way, strive to be the best you can be.
+//If at first you don't succeed, try and try again.
+//Treat the ones you love the way you would want to be treated.
+//Nothing is more impotant than family, cherish them and love them without compromise.
+
+//You are
+//Your Mum is
+//Your boss is
+
+//a stupid idiot who
+//an amazing person who
+//a sexy person who
+
+//likes to run around naked.
+//wets the bed every night.
+//couldn't organise a piss up in a brewery.
+
+function generateMessage() [
+    
+]
